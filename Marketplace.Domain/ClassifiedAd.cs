53c68f26520c6c36e30a77d836c82d6495daa5cb
Sunny <sunnyggadekar@gmail.com>
2022-06-25 20:11:03 +0530

    first design of entity 1-public Id and private fields

---
+++ Marketplace.Domain/ClassifiedAd.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketplace.Domain
+{
+    public class ClassifiedAd
+    {
+        public Guid Id { get; private set; }
+
+        private Guid _ownerId;
+        private string _tittle;
+        private string _text;
+        private decimal _price;
+
+    }
+}
