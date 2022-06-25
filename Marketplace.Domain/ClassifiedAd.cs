4ba10d842fdeff8a382d738651ce8ca45eb6e8c5
Sunny <sunnyggadekar@gmail.com>
2022-06-25 22:02:36 +0530

    4. Added value object for ownerId

--- Marketplace.Domain/ClassifiedAd.cs
+++ Marketplace.Domain/ClassifiedAd.cs
@@ -8,30 +8,22 @@ namespace Marketplace.Domain
 {
     public class ClassifiedAd
     {
-        //only one public field.
-        public Guid Id { get; private set; }
+        public Guid Id { get; }
+        private readonly UserId _ownerId;
 
-        //while creating identity Id must be supplied.
-        public ClassifiedAd(Guid id, Guid ownerId) 
+        public ClassifiedAd(Guid id, UserId ownerId) 
         {
             if (id == default)
                 throw new ArgumentException("Identity must be specified",nameof(id));
             Id = id;
 
-            if (ownerId == default)
-                throw new ArgumentException("Owner id must be specified", nameof(ownerId));
             _ownerId = ownerId;
         }
 
-        //Adding Behavior.
         public void SetTitle(string title) => _tittle = title;
-
         public void UpdateText(string text) => _text = text;
-
         public void UpdatePrice(decimal price) => _price = price;
 
-        //rest are all priavte properties to take advantage of encapsulation.
-        private Guid _ownerId;
         private string _tittle;
         private string _text;
         private decimal _price;
---
+++ Marketplace.Domain/UserId.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketplace.Domain
+{
+    public class UserId
+    {
+        private readonly Guid _value;
+
+        public UserId(Guid value)
+        {
+            if (value == default)
+                throw new ArgumentException("User id cannot be empty", nameof(value));
+
+            _value = value;
+        }
+    }
+}
