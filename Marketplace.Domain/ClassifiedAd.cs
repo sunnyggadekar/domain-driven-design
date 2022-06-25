c6f2e620f9d5ffd502bbdf3fa1d99653dfd3dc0b
Sunny <sunnyggadekar@gmail.com>
2022-06-25 20:20:12 +0530

    added contructor for the entity to be instantiated

--- Marketplace.Domain/ClassifiedAd.cs
+++ Marketplace.Domain/ClassifiedAd.cs
@@ -9,6 +9,12 @@ namespace Marketplace.Domain
     public class ClassifiedAd
     {
         public Guid Id { get; private set; }
+        public ClassifiedAd(Guid id)
+        {
+            if (id == default)
+                throw new ArgumentException("Identity must be specified",nameof(id));
+            Id = id;
+        }
 
         private Guid _ownerId;
         private string _tittle;
