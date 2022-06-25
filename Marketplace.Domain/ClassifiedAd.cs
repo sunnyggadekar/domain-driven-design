71a928f3c591346c333601f9c44f6c707bc27937
Sunny <sunnyggadekar@gmail.com>
2022-06-25 21:00:55 +0530

    Added behavior to

--- Marketplace.Domain/ClassifiedAd.cs
+++ Marketplace.Domain/ClassifiedAd.cs
@@ -8,13 +8,23 @@ namespace Marketplace.Domain
 {
     public class ClassifiedAd
     {
-        public Guid Id { get; private set; } //only one public field
-        public ClassifiedAd(Guid id) //while creating only entity Id is public available
+        //only one public field
+        public Guid Id { get; private set; }
+
+        //while creating only entity Id is public available
+        public ClassifiedAd(Guid id) 
         {
             if (id == default)
                 throw new ArgumentException("Identity must be specified",nameof(id));
             Id = id;
         }
+
+        //Adding Behavior
+        public void SetTitle(string title) => _tittle = title;
+
+        public void UpdateText(string text) => _text = text;
+
+        public void UpdatePrice(decimal price) => _price = price;
 
         //rest are all priavte properties to take advantage of encapsulation
         private Guid _ownerId;
