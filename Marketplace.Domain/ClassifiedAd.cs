d6b6e7ad1a78bada8c1181ce30a801ae638d712b
Sunny <sunnyggadekar@gmail.com>
2022-06-25 21:18:50 +0530

    3. Ensuring correctness - constraints for input

--- Marketplace.Domain/ClassifiedAd.cs
+++ Marketplace.Domain/ClassifiedAd.cs
@@ -8,25 +8,29 @@ namespace Marketplace.Domain
 {
     public class ClassifiedAd
     {
-        //only one public field
+        //only one public field.
         public Guid Id { get; private set; }
 
-        //while creating only entity Id is public available
-        public ClassifiedAd(Guid id) 
+        //while creating identity Id must be supplied.
+        public ClassifiedAd(Guid id, Guid ownerId) 
         {
             if (id == default)
                 throw new ArgumentException("Identity must be specified",nameof(id));
             Id = id;
+
+            if (ownerId == default)
+                throw new ArgumentException("Owner id must be specified", nameof(ownerId));
+            _ownerId = ownerId;
         }
 
-        //Adding Behavior
+        //Adding Behavior.
         public void SetTitle(string title) => _tittle = title;
 
         public void UpdateText(string text) => _text = text;
 
         public void UpdatePrice(decimal price) => _price = price;
 
-        //rest are all priavte properties to take advantage of encapsulation
+        //rest are all priavte properties to take advantage of encapsulation.
         private Guid _ownerId;
         private string _tittle;
         private string _text;
