dff17a89f3f0d649f94efbf5475f9774e0b0f3c3
Sunny <sunnyggadekar@gmail.com>
2022-06-26 15:40:43 +0530

    8. immutability of money value object

--- Marketplace.Domain/Money.cs
+++ Marketplace.Domain/Money.cs
@@ -8,5 +8,14 @@ namespace Marketplace.Domain
         public decimal Amount { get; }
 
         public Money(decimal amount) => Amount = amount;
+
+        public Money Add(Money summand) => new Money(Amount + summand.Amount);
+
+        public Money Subtract(Money subtrahend) => new Money(Amount - subtrahend.Amount);
+
+        public static Money operator +(Money summand1, Money summand2) => summand1.Add(summand2);
+
+        public static Money operator -(Money subtrahend1, Money subtrahend2) => subtrahend1.Subtract(subtrahend2);
+
     }
 }
