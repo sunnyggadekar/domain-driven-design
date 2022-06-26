349276709fedd8414f59522882c1eb36880a30d0
Sunny <sunnyggadekar@gmail.com>
2022-06-26 15:31:45 +0530

    Added price value object with a domain rule

---
+++ Marketplace.Domain/Price.cs
@@ -0,0 +1,14 @@
+﻿using Marketplace.Framework;
+using System;
+
+namespace Marketplace.Domain
+{
+    public class Price : Money
+    {
+        public Price(decimal amount) : base(amount)
+        {
+            if (amount < 0)
+                throw new ArgumentException("Price cannot be negative", nameof(amount));
+        }
+    }
+}
