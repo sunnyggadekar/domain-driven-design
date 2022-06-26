ee0cfe818360faaccb0723e84b390941b58935bf
Sunny <sunnyggadekar@gmail.com>
2022-06-26 15:21:00 +0530

    Added a abstract base class for value objects

--- Marketplace.Domain/Money.cs
+++ Marketplace.Domain/Money.cs
@@ -1,27 +1,12 @@
-﻿using System;
+﻿using Marketplace.Framework;
+using System;
 
 namespace Marketplace.Domain
 {
-    public class Money : IEquatable<Money>
+    public class Money : Value<Money>
     {
         public decimal Amount { get; }
 
         public Money(decimal amount) => Amount = amount;
-
-        public bool Equals(Money other)
-        {
-            if (ReferenceEquals(null, other)) return false;
-            if (ReferenceEquals(this, other)) return true;
-
-            return Amount.Equals(other.Amount);
-        }
-
-        public override int GetHashCode() => Amount.GetHashCode();
-
-        public static bool operator ==(Money left, Money right) => Equals(left, right);
-
-        public static bool operator !=(Money left, Money right) => !Equals(left, right);
-
-
     }
 }
