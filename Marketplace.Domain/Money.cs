74eb85e4cf37caef899c5481e6675449a28101ae
Sunny <sunnyggadekar@gmail.com>
2022-07-10 21:33:51 +0530

    11. Added currency code to money

--- Marketplace.Domain/Money.cs
+++ Marketplace.Domain/Money.cs
@@ -5,28 +5,50 @@ namespace Marketplace.Domain
 {
     public class Money : Value<Money>
     {
-        public static Money FromDecimal(decimal amount) => new(amount);
 
-        public static Money FormString(string amount) => new(decimal.Parse(amount));
+        private const string DefaultCurrency = "EUR";
+        public static Money FromDecimal(decimal amount, string currency = DefaultCurrency) => new(amount, currency);
 
-        protected Money(decimal amount)
+        public static Money FormString(string amount, string currency = DefaultCurrency) => new(decimal.Parse(amount), currency);
+
+        protected Money(decimal amount, string currencyCode = "EUR")
         {
             if (decimal.Round(amount, 2) != amount)
                 throw new ArgumentOutOfRangeException(
                     nameof(amount),
                     "Amount cannot have more than two decimals");
             Amount = amount;
+            CurrencyCode = currencyCode;
         }
 
         public decimal Amount { get; }
+        public string CurrencyCode { get; }
 
-        public Money Add(Money summand) => new Money(Amount + summand.Amount);
+        public Money Add(Money summand)
+        {
+            if (CurrencyCode != summand.CurrencyCode)
+                throw new CurrencyMismatchException("Cannot Add amounts with different currency");
+            return new(Amount + summand.Amount);
+        }
 
-        public Money Subtract(Money subtrahend) => new Money(Amount - subtrahend.Amount);
+        public Money Subtract(Money subtrahend)
+        {
+            if (CurrencyCode != subtrahend.CurrencyCode)
+                throw new CurrencyMismatchException("Cannot subtract amounts with different currency");
+            return new Money(Amount - subtrahend.Amount);
+        }
 
         public static Money operator +(Money summand1, Money summand2) => summand1.Add(summand2);
 
         public static Money operator -(Money subtrahend1, Money subtrahend2) => subtrahend1.Subtract(subtrahend2);
 
+    }
+
+    public class CurrencyMismatchException : Exception
+    {
+        public CurrencyMismatchException(string message) : base(message)
+        {
+
+        }
     }
 }
