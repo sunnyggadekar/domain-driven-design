b18c44e725673e351798e81849c2ba4755820d83
Sunny <sunnyggadekar@gmail.com>
2022-07-24 18:12:26 +0530

    16. Raising events

--- Marketplace.Domain/ClassifiedAd.cs
+++ Marketplace.Domain/ClassifiedAd.cs
@@ -1,14 +1,9 @@
-﻿namespace Marketplace.Domain
-{
-    public class ClassifiedAd
-    {
-        public ClassifiedAd(ClassifiedAdId id, UserId ownerId)
-        {
-            Id = id;
-            OwnerId = ownerId;
-            State = ClassifiedAdState.InActive;
-        }
+﻿using Marketplace.Framework;
 
+namespace Marketplace.Domain
+{
+    public class ClassifiedAd : Entity
+    {
         public ClassifiedAdId Id { get; }
         public UserId OwnerId { get; }
         public ClassifiedAdTitle Title { get; private set; }
@@ -16,6 +11,22 @@
         public Price Price { get; private set; }
         public ClassifiedAdState State { get; private set; }
         public UserId ApprovedBy { get; private set; }
+
+        public ClassifiedAd(ClassifiedAdId id, UserId ownerId)
+        {
+            Id = id;
+            OwnerId = ownerId;
+
+            EnsureValidState();
+
+            Raise(new Events.ClassifiedAdCreated
+            {
+                Id = id,
+                OwnerId = ownerId
+            });
+
+            State = ClassifiedAdState.InActive;
+        }
 
         public enum ClassifiedAdState
         {
@@ -28,24 +39,43 @@
         public void SetTitle(ClassifiedAdTitle title)
         {
             Title = title;
+            Raise(new Events.ClassifiedAdTitleChanged
+            {
+                Id = Id,
+                Tittle = title
+            });
             EnsureValidState();
         }
 
         public void UpdateText(ClassifiedAdText text)
         {
             Text = text;
+            Raise(new Events.ClassifiedAdTextUpdated
+            {
+                Id = Id,
+                AdText = text
+            });
             EnsureValidState();
         }
 
         public void UpdatePrice(Price price)
         {
             Price = price;
+            Raise(new Events.ClassifiedAdPriceUpdated
+            {
+                Id = Id,
+                Price = price
+            });
             EnsureValidState();
         }
 
         public void RequestToPublish()
         {
             State = ClassifiedAdState.PendingReview;
+            Raise(new Events.ClassifiedAdSentForReview
+            {
+                Id = Id
+            });
             EnsureValidState();
         }
 
