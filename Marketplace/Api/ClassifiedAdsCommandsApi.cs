944b247c3c95b8b891221cc09ee63a7ddb047635
Sunny <sunnyggadekar@gmail.com>
2022-09-25 14:39:49 +0530

    21. Application service

--- Marketplace/Api/ClassifiedAdsCommandsApi.cs
+++ Marketplace/Api/ClassifiedAdsCommandsApi.cs
@@ -6,9 +6,18 @@ namespace Marketplace.Api
     [Route("/ad")]
     public class ClassifiedAdsCommandApi : Controller
     {
+        private readonly ClassifiedAdsApplicationService _applicationService;
+
+        public ClassifiedAdsCommandApi(ClassifiedAdsApplicationService applicationService)
+        {
+            _applicationService = applicationService;
+        }
+
+
         [HttpPost]
         public async Task<IActionResult> Post(Contracts.ClassifiedAds.V1.Create request)
         {
+            _applicationService.Handle(request);
             return Ok();
         }
     }
