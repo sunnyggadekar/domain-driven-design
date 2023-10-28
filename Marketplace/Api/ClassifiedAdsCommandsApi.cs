58ec16ac3574c83de8a7082429c7203badbc99a5
Sunny <sunnyggadekar@gmail.com>
2023-10-28 12:56:06 +0530

    23. Contracts and endpoints

--- Marketplace/Api/ClassifiedAdsCommandsApi.cs
+++ Marketplace/Api/ClassifiedAdsCommandsApi.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using static Marketplace.Contracts.ClassifiedAds;
 
 namespace Marketplace.Api
 {
@@ -15,7 +16,35 @@ namespace Marketplace.Api
 
 
         [HttpPost]
-        public async Task<IActionResult> Post(Contracts.ClassifiedAds.V1.Create request)
+        public async Task<IActionResult> Post(V1.Create request)
+        {
+            await _applicationService.Handle(request);
+            return Ok();
+        }
+
+        [HttpPut("name")]
+        public async Task<IActionResult> Put(V1.SetTitle request)
+        {
+            await _applicationService.Handle(request);
+            return Ok();
+        }
+
+        [HttpPut("text")]
+        public async Task<IActionResult> Put(V1.UpdateText request)
+        {
+            await _applicationService.Handle(request);
+            return Ok();
+        }
+
+        [HttpPut("price")]
+        public async Task<IActionResult> Put(V1.UpdatePrice request)
+        {
+            await _applicationService.Handle(request);
+            return Ok();
+        }
+
+        [HttpPut("publish")]
+        public async Task<IActionResult> Put(V1.RequestToPublish request)
         {
             await _applicationService.Handle(request);
             return Ok();
